143d88f8edbef304e59ee2632cb66ccb5fcc4efe
juanpnvd <103511484+juanpnvd@users.noreply.github.com>
2022-12-28 19:31:45 -0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,26 @@
+import praw
+from whatever import tosubs
+import random
+import os
+from keep_alive import keep_alive
+
+keep_alive()
+
+
+reddit = praw.Reddit(client_id=os.environ['client_id'],
+                     client_secret=os.environ['client_secret'],
+                     username=os.environ['username'],
+                     password=os.environ['password'],
+                     user_agent=os.environ['user_agent'])
+
+#get the original post
+subreddit = reddit.subreddit('PetsareAmazing')
+for submission in subreddit.new(limit=1):
+    print("Title: ", submission.title)
+    print("Text: ", submission.selftext)
+
+target_subreddit=random.choice(tosubs)
+
+#crosspost to another subreddit
+submission.crosspost(target_subreddit)
+print("Crossposted to ", target_subreddit)
